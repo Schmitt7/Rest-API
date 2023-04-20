0f5820edfa8bcf187cc6f5d94038b0be91ac146f
Schmitt7 <77367221+Schmitt7@users.noreply.github.com>
2023-04-20 10:39:34 -0400

    Added a get function to grab a home and add it to a list of home objects

--- Rest-API/Controllers/HomeController.cs
+++ Rest-API/Controllers/HomeController.cs
@@ -3,14 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
 
 namespace Rest_API.Controllers
 {
+    [Route("api/[controller]")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+
+
+
+        //Gets all homes and adds them to a list of home objects
+
+        public List<Home> Get() {
+
+            DBConnect objDB = new DBConnect();
+            DataSet ds = objDB.GetDataSet("SELECT * FROM TP_Homes");
+            List<Home> homes = new List<Home>();
+            Home home;
+
+            foreach (DataRow record in ds.Tables[0].Rows) {
+                home = new Home();
+                home.HomeID = int.Parse(record["HomeID"].ToString());
+                //Add more code similar to above for grabbing home
+                //from a database and saving as home object
+            }
+            return homes;
         }
     }
 }
